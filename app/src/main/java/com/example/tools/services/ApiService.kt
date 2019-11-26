a205ce81969f6165789a9e139b0a94f1ce6d032b
Pedro Silveira <peagah@gmail.com>
2019-11-26 18:18:43 -0400

    Versionando com a aula do dia 26 de novembro de 2019

--- app/src/main/java/com/example/tools/services/ApiService.kt
+++ app/src/main/java/com/example/tools/services/ApiService.kt
@@ -1,11 +1,22 @@
 package com.example.tools.services
 
-import com.example.tools.models.Joke
-import retrofit2.Call
-import retrofit2.http.GET
+import com.example.tools.models.Movie
+import io.reactivex.Completable
+import io.reactivex.Observable
+import io.reactivex.Single
+import retrofit2.http.*
 
 interface ApiService {
 
-    @GET("/random_ten")
-    fun get(): Call<MutableList<Joke>>
+    @GET("/movies.json")
+    fun get(): Observable<Map<String, Movie>>
+
+    @PUT("/movies/{id}.json")
+    fun put(@Path("id") id: String, @Body movie: Movie): Single<Movie>
+
+    @DELETE("/movies/{id}.json")
+    fun delete(@Path("id") id: String): Completable
+
+    @POST("/movies.json")
+    fun post(@Body movie: Movie): Single<String>
 }
